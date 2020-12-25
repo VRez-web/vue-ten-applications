d0a5128edb0f37162e5dc86326738fe015481ca1
VRez-web <56605107+VRez-web@users.noreply.github.com>
2020-12-25 18:16:06 +0300

    step by step

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <header class="header">
+    <nav class="header__nav">
+        <a href="#"> DC Heroes</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style  scoped>
+.header {
+  width: 100%;
+  padding: 10px 30px;
+  background-color: #fbab7e;
+  background-image: linear-gradient(62deg, #fbab7e 0%, #f7ce68 100%);
+  color:#fff;
+  font-size: 20px;
+
+}
+.header a{
+  text-decoration: none;
+   color:#fff;
+}
+</style>
---
+++ src/components/DcHeroes.vue
@@ -0,0 +1,49 @@
+<template>
+  <h1>Dc Heroes {{ countHeroes }}</h1>
+  <ul>
+    <li v-for="(hero, index) in dcHeroes" :key="hero.name">
+      <p>{{ hero.name }} <span @click="remove(index)">x</span></p>
+    </li>
+  </ul>
+  <form @submit.prevent="addHero">
+    <input type="text" v-model="newHero" placeholder="Type name hero here" />
+    <button>Add Hero</button>
+  </form>
+</template>
+
+<script>
+export default {
+ data() {
+    return {
+      newHero: "",
+      dcHeroes: [
+        { name: "SuperGirl" },
+        { name: "Flash" },
+        { name: "Arrow" },
+        { name: "Superman" },
+        { name: "Batman" },
+      ],
+    };
+  },
+    methods: {
+    addHero() {
+      if (this.newHero !== "") {
+        this.dcHeroes.push({ name: this.newHero });
+        this.newHero = "";
+      }
+    },
+    remove(index) {
+      this.dcHeroes = this.dcHeroes.filter((hero, i) => i != index);
+    },
+  },
+  computed: {
+    countHeroes() {
+      return this.dcHeroes.length;
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
